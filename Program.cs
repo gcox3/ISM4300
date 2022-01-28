b7cda5e61771e8ae7586cb4c9352e0c790611536
gcox3 <98573591+gcox3@users.noreply.github.com>
2022-01-28 08:12:17 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Programmer Name: Gabrielle Cox
+using System;
+
+namespace TechAssignment2.BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is one of my favorite quotes: 'I haven't failed, I have just found 10,000 ways that won't work.' - Thomas Edison. Submitted by Gabi Cox");
+        }
+    }
+}
